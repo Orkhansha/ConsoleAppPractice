3b82c3232137c63c8074adc7cd19e111899c02c0
Orkhansha <orkhansha@code.edu.az>
2022-07-19 01:42:01 +0400

    Added LibraryApp files

---
+++ LibraryApp/LibraryApp/Program.cs
@@ -0,0 +1,74 @@
+﻿using Domain.Models;
+using Service.Services;
+using Service.Services.Helpers;
+using System;
+
+namespace LibraryApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LibraryService libraryService = new LibraryService();
+            Helper.WriteConsole(ConsoleColor.Yellow, "Select one option:");
+            Helper.WriteConsole(ConsoleColor.Green, "1 - Create library, 2 - Get library by Id, 3 - Update library, 4 - Delete library");
+
+            while (true)
+            {
+                SelectOption: string SelectOption = Console.ReadLine();
+                int selectTrueOption;
+                bool isSelectOption = int.TryParse(SelectOption, out selectTrueOption);
+                if (isSelectOption)
+                {
+                    switch (selectTrueOption)
+                    {
+                        case 1:
+                            Helper.WriteConsole(ConsoleColor.Yellow, "Add library name:");
+                            string labraryName = Console.ReadLine();
+
+                            Helper.WriteConsole(ConsoleColor.Yellow, "Add seat count:");
+                            SeatCount: string librarySeatCount = Console.ReadLine();
+                            int seatCount;
+                            bool isSeatCount = int.TryParse(librarySeatCount, out seatCount);
+                            if (isSeatCount)
+                            {
+                                Library library = new Library
+                                {
+                                    Name = labraryName,
+                                    SeatCount = seatCount
+
+                                };
+                                var result = libraryService.Create(library);
+                                Helper.WriteConsole(ConsoleColor.Green, $"Library Id: {result.Id}, Library name: {result.Name}, Library seat count: {result.SeatCount}");
+
+                            }
+                            else
+                            {
+                                Helper.WriteConsole(ConsoleColor.Red, "Add correct seat count:");
+                                goto SeatCount;
+                            }
+
+                            break;
+                        case 2:
+                            Console.WriteLine();
+                            break;
+                        case 3:
+                            Console.WriteLine();
+                            break;
+                        case 4:
+                            Console.WriteLine();
+                            break;
+                        default:
+                            Helper.WriteConsole(ConsoleColor.Red, "Select correct option number:");
+                            break;
+                    }
+                }
+                else
+                {
+                    Helper.WriteConsole(ConsoleColor.Red, "Select correct option:");
+                    goto SelectOption;
+                }
+            }
+        }
+    }
+}
---
+++ LibraryApp/Service/Services/LibraryService.cs
@@ -0,0 +1,45 @@
+﻿
+using Domain.Models;
+using Repository.Repostories;
+using Service.Services.Interfaces;
+using System.Collections.Generic;
+
+namespace Service.Services
+{
+    public class LibraryService : ILibraryService
+    {
+        private LibraryRepository _libraryRepository;
+        private int _count;
+        public LibraryService()
+        {
+            _libraryRepository = new LibraryRepository();
+        }
+        public Library Create(Library library)
+        {
+            library.Id = _count;
+            _libraryRepository.Create(library);
+            _count++;
+            return library;
+        }
+
+        public void Delete(int Id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public List<Library> GetAll()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Library GetById(int Id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Library Update(int Id, Library library)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
